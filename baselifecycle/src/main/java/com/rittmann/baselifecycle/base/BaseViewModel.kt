8669201a69ca430bccab3f06bfb499b1be680dba
Rittmann <willirittmann@outlook.com>
2020-12-22 23:25:01 -0300

    Generic executeAsync

--- baselifecycle/src/main/java/com/rittmann/baselifecycle/base/BaseViewModel.kt
+++ baselifecycle/src/main/java/com/rittmann/baselifecycle/base/BaseViewModel.kt
@@ -4,9 +4,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rittmann.baselifecycle.livedata.SingleLiveEvent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 open class BaseViewModel : ViewModel() {
 
+    protected var viewModelScopeGen: CoroutineScope? = null
     protected var _progress = MutableLiveData<Boolean>()
     protected var _errorCon = SingleLiveEvent<Void>()
     protected var _errorGen = SingleLiveEvent<Void>()
@@ -34,6 +41,34 @@ open class BaseViewModel : ViewModel() {
     open fun clearViewModel() {
         _errorCon = SingleLiveEvent()
         _errorGen = SingleLiveEvent()
+    }
+
+    fun executeAsyncProgress(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        scope: CoroutineScope = GlobalScope,
+        block: suspend () -> Unit
+    ) {
+        val s = viewModelScopeGen ?: scope
+        s.launch {
+            withContext(dispatcher) {
+                _progress.postValue(true)
+                block()
+                _progress.postValue(false)
+            }
+        }
+    }
+
+    fun executeAsync(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        scope: CoroutineScope = GlobalScope,
+        block: suspend () -> Unit
+    ) {
+        val s = viewModelScopeGen ?: scope
+        s.launch {
+            withContext(dispatcher) {
+                block()
+            }
+        }
     }
 }
 
