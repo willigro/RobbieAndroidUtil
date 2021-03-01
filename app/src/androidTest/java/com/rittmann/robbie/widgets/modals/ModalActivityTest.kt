28aed7f33b50383f79eb284b3b8169c3a157d59c
Willi Rittmann <willirittmann@outlook.com>
2021-03-01 16:10:53 -0300

    Testing: configure the default layout

--- app/src/androidTest/java/com/rittmann/robbie/widgets/modals/ModalActivityTest.kt
+++ app/src/androidTest/java/com/rittmann/robbie/widgets/modals/ModalActivityTest.kt
@@ -6,6 +6,7 @@ import com.rittmann.robbie.support.ActivityTest
 import com.rittmann.robbie.support.EspressoUtil.backgroundColor
 import com.rittmann.robbie.support.EspressoUtil.checkValue
 import com.rittmann.widgets.dialog.ModalInternal
+import com.rittmann.widgets.dialog.ModalUtil
 import com.rittmann.widgets.dialog.modal
 import org.junit.Test
 
@@ -67,7 +68,7 @@ class ModalActivityTest : ActivityTest() {
         val cancelText = "Text cancel"
         val concludeText = "Text conclude"
 
-        getActivity<ModalActivity>().onActivity {
+        val activity = getActivity<ModalActivity>().onActivity {
             val internalChangeLayout = ModalInternal(
                 context = it,
                 title = title,
@@ -80,7 +81,7 @@ class ModalActivityTest : ActivityTest() {
                 resIdMessage = R.id.new_modal_message,
                 resIdBtnCancel = R.id.new_btn_cancel,
                 resIdBtnConclude = R.id.new_btn_conclude,
-                useResJustInThisModal = true
+                useResJustInThisModal = true // don`t configure this id that default
             )
 
             it.modal(internalChangeLayout)
@@ -92,7 +93,7 @@ class ModalActivityTest : ActivityTest() {
         checkValue(R.id.new_btn_conclude, concludeText)
         backgroundColor(R.id.new_modal_layout, android.R.color.holo_orange_dark)
 
-        getActivity<ModalActivity>().onActivity {
+        activity.onActivity {
             val internalDefaultLayout = ModalInternal(
                 context = it,
                 title = title,
@@ -110,5 +111,61 @@ class ModalActivityTest : ActivityTest() {
         checkValue(R.id.btnCancel, cancelText)
         checkValue(R.id.btnConclude, concludeText)
         backgroundColor(R.id.dialogLayout, android.R.color.white)
+    }
+
+    @Test
+    fun showModal_SetTheDefaultLayout_UsingInternalModal() {
+        val title = "modal title"
+        val message = "modal Message"
+        val cancelText = "Text cancel"
+        val concludeText = "Text conclude"
+
+        var modalOne: ModalUtil? = null
+
+        val activity = getActivity<ModalActivity>().onActivity {
+            val internalChangeTheDefaultLayout = ModalInternal(
+                context = it,
+                title = title,
+                message = message,
+                cancelText = cancelText,
+                concludeText = concludeText,
+                show = true,
+                resIdLayout = R.layout.modal_layout_with_new_ids,
+                resIdTitle = R.id.new_modal_title,
+                resIdMessage = R.id.new_modal_message,
+                resIdBtnCancel = R.id.new_btn_cancel,
+                resIdBtnConclude = R.id.new_btn_conclude,
+                useResJustInThisModal = false // configure the resId as default
+            )
+
+            modalOne = it.modal(internalChangeTheDefaultLayout)
+        }
+
+        checkValue(R.id.new_modal_title, title)
+        checkValue(R.id.new_modal_message, message)
+        checkValue(R.id.new_btn_cancel, cancelText)
+        checkValue(R.id.new_btn_conclude, concludeText)
+        backgroundColor(R.id.new_modal_layout, android.R.color.holo_orange_dark)
+
+        modalOne?.dismiss()
+
+        activity.onActivity {
+            val internalDefaultLayout = ModalInternal(
+                context = it,
+                title = title,
+                message = message,
+                cancelText = cancelText,
+                concludeText = concludeText,
+                show = true
+            )
+
+            it.modal(internalDefaultLayout)
+        }
+
+        checkValue(R.id.new_modal_title, title)
+        checkValue(R.id.new_modal_message, message)
+        checkValue(R.id.new_btn_cancel, cancelText)
+        checkValue(R.id.new_btn_conclude, concludeText)
+        backgroundColor(R.id.new_modal_layout, android.R.color.holo_orange_dark)
     }
 }
