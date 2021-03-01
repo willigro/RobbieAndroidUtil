128a26615175beb811fd0db1a5ae4f4376ad1478
Willi Rittmann <willirittmann@outlook.com>
2021-03-01 12:20:48 -0300

    Testing: show modal using the internal object

--- app/src/androidTest/java/com/rittmann/robbie/widgets/modals/ModalActivityTest.kt
+++ app/src/androidTest/java/com/rittmann/robbie/widgets/modals/ModalActivityTest.kt
@@ -4,6 +4,7 @@ import com.rittmann.robbie.ModalActivity
 import com.rittmann.robbie.R
 import com.rittmann.robbie.support.ActivityTest
 import com.rittmann.robbie.support.EspressoUtil.checkValue
+import com.rittmann.widgets.dialog.ModalInternal
 import com.rittmann.widgets.dialog.modal
 import org.junit.Test
 
@@ -24,6 +25,32 @@ class ModalActivityTest : ActivityTest() {
                 concludeText = concludeText,
                 show = true
             )
+        }
+
+        checkValue(R.id.dialogTitleTextView, title)
+        checkValue(R.id.dialogSubtitleTextView, message)
+        checkValue(R.id.btnCancel, cancelText)
+        checkValue(R.id.btnConclude, concludeText)
+    }
+
+    @Test
+    fun showModal_WithoutChangeTheLayout_UsingInternalModal() {
+        val title = "modal title"
+        val message = "modal Message"
+        val cancelText = "Text cancel"
+        val concludeText = "Text conclude"
+
+        getActivity<ModalActivity>().onActivity {
+            val internal = ModalInternal(
+                context = it,
+                title = title,
+                message = message,
+                cancelText = cancelText,
+                concludeText = concludeText,
+                show = true
+            )
+
+            it.modal(internal)
         }
 
         checkValue(R.id.dialogTitleTextView, title)
