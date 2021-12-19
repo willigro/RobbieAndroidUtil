26966d96a55abfda5970531556ceeb6a3d74391b
Rittmann <willirittmann@outlook.com>
2021-12-19 11:39:20 -0300

    Update versions
    Signed-off-by: Rittmann <willirittmann@outlook.com>

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -20,7 +20,7 @@ object Versions {
 
     // App version
     const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.3.1.2"
+    const val VERSION_NAME = "1.3.1.3"
 
     // Appcompat
     const val APPCOMPAT = "1.3.1"
